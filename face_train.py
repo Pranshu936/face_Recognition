66deefbf9bd2ecff5a60521e1e5659a7e90d30ca
Pranshu kumar <123887342+Pranshu936@users.noreply.github.com>
2024-11-26 11:00:15 +0530

    Update face_train.py

--- face_train.py
+++ face_train.py
@@ -3,55 +3,23 @@ import numpy as np
 from PIL import Image
 import os
 
+def get_images_and_labels(path):
+    image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+    face_samples, ids = [], []
+    for image_path in image_paths:
+        img = np.array(Image.open(image_path).convert('L'), 'uint8')
+        id = int(os.path.split(image_path)[-1].split("-")[1])
+        faces = cv2.CascadeClassifier("haarcascade_frontalface_default.xml").detectMultiScale(img)
+        for (x, y, w, h) in faces:
+            face_samples.append(img[y:y+h, x:x+w])
+            ids.append(id)
+    return face_samples, ids
 
 if __name__ == "__main__":
-    
-    # Directory path where the face images are stored.
     path = './images/'
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     print("\n[INFO] Training...")
-    # Haar cascade file for face detection
-    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    
-    def getImagesAndLabels(path):
-        """
-        Load face images and corresponding labels from the given directory path.
-    
-        Parameters:
-            path (str): Directory path containing face images.
-    
-        Returns:
-            list: List of face samples.
-            list: List of corresponding labels.
-        """
-        imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
-        faceSamples = []
-        ids = []
-    
-        for imagePath in imagePaths:
-            # Convert image to grayscale
-            PIL_img = Image.open(imagePath).convert('L')
-            img_numpy = np.array(PIL_img, 'uint8')
-            
-            # Extract the user ID from the image file name
-            id = int(os.path.split(imagePath)[-1].split("-")[1])
-    
-            # Detect faces in the grayscale image
-            faces = detector.detectMultiScale(img_numpy)
-    
-            for (x, y, w, h) in faces:
-                # Extract face region and append to the samples
-                faceSamples.append(img_numpy[y:y+h, x:x+w])
-                ids.append(id)
-    
-        return faceSamples, ids
-    
-    faces, ids = getImagesAndLabels(path)
-    
-    # Train the recognizer with the face samples and corresponding labels
+    faces, ids = get_images_and_labels(path)
     recognizer.train(faces, np.array(ids))
-    
-    # Save the trained model into the current directory
     recognizer.write('trainer.yml')
-    
     print("\n[INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
